1e72dd70b8d83751c2365b1f7ecc2b44b5fa9eac
Andrés Miranda <31228376+andresmiranda2000@users.noreply.github.com>
2024-01-04 16:42:27 +0000

    cards

--- src/js/component/cards.jsx
+++ src/js/component/cards.jsx
@@ -1,24 +1,25 @@
 import React from "react";
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
 
 export const Card = (prop) => {
     return (
-        <div className="card my-3 h-100">
+        <div className="card my-3">
             <img className="card-img-top" src={prop.image} alt="Image"/>
             <div className="card-body text-center">
                 <h5 className="card-title">{prop.title}</h5>
                 <p className="card-text">{prop.description}</p>
+                <a href={prop.buttonUrl} className="btn btn-primary">{prop.buttonLabel}</a>
             </div>
-                <ul class="list-group list-group-flush text-center">
-                    <li class="list-group-item"><a href={prop.buttonUrl} className="btn btn-primary">{prop.buttonLabel}</a></li>
-                </ul>
-         </div>)
-}
+        </div>
+    );
+};
 
 Card.propTypes = {
     image: PropTypes.string,
-    title:PropTypes.string,
-    description:PropTypes.string,
-    buttonUrl:PropTypes.string,
-    buttonLabel:PropTypes.string,
-}
+    title: PropTypes.string,
+    description: PropTypes.string,
+    buttonUrl: PropTypes.string,
+    buttonLabel: PropTypes.string,
+};
+
+
